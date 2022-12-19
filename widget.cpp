865fa475929b9affc7b8da7e075b6d785d958852
Kirik516 <kirik516@mail.ru>
2022-12-19 04:46:17 +0300

    Rebuild draw widget for using draw stratagy

--- widget.cpp
+++ widget.cpp
@@ -2,95 +2,45 @@
 
 void Widget::paintEvent(QPaintEvent *)
 {
-    QVector<QPen> penForPoint = QVector<QPen>(2);
-    penForPoint[0] = QPen(Qt::red);
-    penForPoint[0].setWidth(3);
-    penForPoint[1] = QPen(Qt::blue);
-    penForPoint[1].setWidth(3);
-
+    QStyleOption opt;
+    opt.init(this);
     QPainter p(this);
-    p.setPen(QPen(Qt::black));
+    style()->drawPrimitive(QStyle::PE_Widget, &opt, &p, this);
 
-    int size = this->triangle.size();
-    for(int i = 0; i < size; i++)
-    {
-        p.drawLine(this->triangle[i], this->triangle[(i + 1)%size]);
-    }
-
-    if (this->pinPoint.x() > -1)
-    {
-        p.setPen(penForPoint[this->isInside]);
-        p.drawPoint(this->pinPoint);
-    }
+    p.setRenderHint(QPainter::Antialiasing, true);
+    this->algo->draw(p);
 }
 
 void Widget::mouseReleaseEvent(QMouseEvent *event)
 {
-    this->pinPoint = event->pos();
-    this->isInside = this->isPointInside(this->pinPoint);
+    qDebug() << "Grab point";
+    this->algo->setInespectPoint(event->pos());
+    this->algo->reset();
+    this->algo->forward();
     this->update();
-}
-
-bool Widget::isPointInside(QPoint &p)
-{
-    return vectorProdAlgorithm(p);
-}
-
-// Ax + By + C = 0
-// A1x + B1y + C1 = A2x + B2y + C2
-// A1 B1
-// A2 B2
-// A1*B2 - A2*B1
-bool Widget::lineCrossAlgorithm(QPoint &p)
-{
-    int edgeCrossCount = 0;
-    int dim;
-    QPoint traceNorm(0, 0 - p.y());
-    int size = this->triangle.size();
-    for(int i = 0; i < size; i++)
-    {
-        QPoint edgeNorm(this->triangle[i].y() - this->triangle[(i + 1)%size].y(),
-                        this->triangle[(i + 1)%size].x() - this->triangle[i].x());
-        dim = traceNorm.x() * edgeNorm.y() - edgeNorm.x() * traceNorm.y();
-        edgeCrossCount += (dim < 0);
-    }
-    if (edgeCrossCount == 0 || edgeCrossCount%2 != 0)
-    {
-        return false;
-    }
-    return true;
-}
-
-// i  j   k
-// 33 -14 0
-// 150 40 0
-// 33 * 40 - 150 * 40 = 1320 - 6000 = -4680
-bool Widget::vectorProdAlgorithm(QPoint &p)
-{
-    int rightTripleCount = 0;
-    int z;
-    int size = this->triangle.size();
-    for(int i = 0; i < size; i++)
-    {
-        QPoint pointVector(p.x() - this->triangle[i].x(), p.y() - this->triangle[i].y());
-        QPoint edgeVector(this->triangle[(i + 1)%size].x() - this->triangle[i].x(),
-                        this->triangle[(i + 1)%size].y() - this->triangle[i].y());
-        z = pointVector.x() * edgeVector.y() - pointVector.y() * edgeVector.x();
-        rightTripleCount += (z < 0);
-    }
-    if (rightTripleCount == size)
-    {
-        return true;
-    }
-    return false;
+    this->timer.start();
 }
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
-    , isInside(false)
-    , pinPoint(-1, -1)
 {
-    this->triangle << QPoint(200, 150) << QPoint(350, 190) << QPoint(230, 330);
+    this->timer.setInterval(1000);
+    this->timer.setSingleShot(false);
+    connect(&this->timer, &QTimer::timeout, this, &Widget::timerTick);
 }
 
-Widget::~Widget() {}
+void Widget::setDrawAlgo(QSharedPointer<PaintStratagy> algo)
+{
+    this->algo = algo;
+}
+
+void Widget::timerTick()
+{
+    qDebug() << "tick";
+    this->algo->forward();
+    if (this->algo->isDone())
+    {
+        this->timer.stop();
+    }
+    this->update();
+}
--- widget.h
+++ widget.h
@@ -1,30 +1,36 @@
 #ifndef WIDGET_H
 #define WIDGET_H
 
+#include <QDebug>
+#include <QMap>
 #include <QMouseEvent>
 #include <QPainter>
+#include <QTimer>
+#include <QStyleOption>
 #include <QWidget>
 
+#include "paint-stratagy/paint-stratagy.h"
+
+/*!
+ * \brief The Widget class is the widget-manager of visualization.
+ */
 class Widget : public QWidget
 {
     Q_OBJECT
 
-    enum DrawState {};
+    QTimer timer;
 
-    bool isInside;
-    QPoint pinPoint;
-    QPolygon triangle;
+    QSharedPointer<PaintStratagy> algo;
 
-    virtual void paintEvent(QPaintEvent *);
-    virtual void mouseReleaseEvent(QMouseEvent *event);
-
-    bool isPointInside(QPoint &p);
-    bool lineCrossAlgorithm(QPoint &p);
-    bool vectorProdAlgorithm(QPoint &p);
+    void paintEvent(QPaintEvent *) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
 
 public:
-    Widget(QWidget *parent = 0);
-    ~Widget();
+    explicit Widget(QWidget *parent = 0);
+    void setDrawAlgo(QSharedPointer<PaintStratagy> algo);
+
+private slots:
+    void timerTick();
 };
 
 #endif // WIDGET_H
